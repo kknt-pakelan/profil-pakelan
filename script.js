b4fe42a7b2b1edc5d2b50a3fe0b7ddedf8af7d53
David <davidsatriaf1@gmail.com>
2026-01-25 20:18:30 +0700

    Menambahkan mark tempat

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
-// 1. Inisialisasi Peta
-// Koordinat Tengah Pakelan: -7.816 (Lat), 112.009 (Long)
-// Zoom Level: 16 (Cukup dekat untuk melihat jalan)
+// Inisialisasi Peta
+// Koordinat Tengah Pakelan
 var map = L.map('map').setView([-7.816350, 112.009500], 16);
 
-// 2. Tambahkan Tile Layer (Peta Dasar)
-// Kita pakai OpenStreetMap yang gratis
+// Tile Layer (Peta Dasar)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// 3. Tambahkan Marker (Penanda Lokasi)
-// Marker 1: Kantor Kelurahan Pakelan
-var markerKantor = L.marker([-7.815948, 112.009384]).addTo(map);
+// Marker (Penanda Lokasi)
+// Marker Kantor Kelurahan Pakelan
+var markerKantor = L.marker([-7.8184308552018535, 112.00956899535286]).addTo(map);
 
 // Menambahkan Pop-up saat marker diklik
 markerKantor.bindPopup("<b>Kantor Kelurahan Pakelan</b><br>Pusat Pelayanan Warga.").openPopup();
 
-// Marker 2: Kelenteng Tjoe Hwie Kiong (Contoh Wisata)
-var markerKelenteng = L.marker([-7.817500, 112.008800]).addTo(map);
+// Marker Kelenteng Tjoe Hwie Kiong 
+var markerKelenteng = L.marker([-7.819991724537468, 112.00959693836458]).addTo(map);
 markerKelenteng.bindPopup("<b>Kelenteng Tjoe Hwie Kiong</b><br>Ikon Wisata Heritage.");
+
+// Marker Gereja Pusat Pantekosta Indonesia
+var markerGerejaPusat = L.marker([-7.814269955615778, 112.01172826582454]).addTo(map);
+markerGerejaPusat.bindPopup("<b>Gereja Pusat Pantekosta Indonesia</b>");
+
+// Marker Gereja Bethany Indonesia House of Restoration Kediri
+var markerGerejaBethany = L.marker([-7.818141477013701, 112.00936904771905]).addTo(map);
+markerGerejaBethany.bindPopup("<b>Gereja Bethany Indonesia House of Restoration Kediri</b>");
+
+// Marker Gereja Reformed Injili Indonesia PRII Kediri
+var markerGerejaPRII = L.marker([-7.8160058923517415, 112.00891575272055]).addTo(map);
+markerGerejaPRII.bindPopup("<b>Gereja Reformed Injili Indonesia PRII Kediri</b>");
+
+// Marker GPdl FILADELFIA Kediri Kota
+var markerGerejaFILADELFIA = L.marker([-7.818124753896681, 112.01087353370222]).addTo(map);
+markerGerejaFILADELFIA.bindPopup("<b>GPdlFILADELFIA Kediri Kota</b>");
+
+// Marker Vihara Metta Maitreya
+var markerViharaMettaMaitreya = L.marker([-7.817363176848054, 112.00899966316895]).addTo(map);
+markerViharaMettaMaitreya.bindPopup("<b>Vihara Metta Maitreya</b>");
+
+// Marker Susteran Putri Kasih
+var markerSusteranPutriKasih = L.marker([-7.813714395469712, 112.01022930853844]).addTo(map);
+markerSusteranPutriKasih.bindPopup("<b>Susteran Putri Kasih</b>");
+
+// Marker Gie Kie Kong Soe
+var markerGieKieKongSoe = L.marker([-7.817650780702965, 112.01115682573888]).addTo(map);
+markerGieKieKongSoe.bindPopup("<b>Gie Kie Kong Soe</b>");
+
+// Marker Masjid Al-Hidayah
+var markerMasjidAlHidayah = L.marker([-7.816128419877064, 112.01015643248117]).addTo(map);
+markerMasjidAlHidayah.bindPopup("<b>Masjid Al-Hidayah</b>");
+
+// Marker Punden Pakelan
+var markerPundenPakelan = L.marker([-7.8161937913280415, 112.01017182125483]).addTo(map);
+markerPundenPakelan.bindPopup("<b>Punden Pakelan</b>");
+
+// Marker Koramil 0809/01 Kota Kediri
+var markerKoramil = L.marker([-7.819478296348127, 112.01094422339499]).addTo(map);
+markerKoramil.bindPopup("<b>Koramil 0809/01 Kota Kediri</b>");
 
 // 4. Menampilkan Batas Wilayah (GeoJSON)
 var geojsonLayer = L.geoJSON(pakelanData, {
     style: function(feature) {
         return {
-            color: "#4B24B3",       // Warna Garis (Ungu Pakelan)
-            weight: 3,              // Tebal Garis
-            opacity: 1,             // Kejelasan Garis (1 = Solid)
-            fillColor: "#FDD835",   // Warna Isi (Kuning Aksen)
-            fillOpacity: 0.2        // Transparansi Isi (0.2 = Bening)
+            color: "#4B24B3",       
+            weight: 3,              
+            opacity: 1,             
+            fillColor: "#FDD835",   
+            fillOpacity: 0.2        
         };
     },
     onEachFeature: function(feature, layer) {
@@ -40,6 +78,6 @@ var geojsonLayer = L.geoJSON(pakelanData, {
     }
 }).addTo(map);
 
-// 5. Auto Zoom ke Wilayah Desa
+// Auto Zoom ke Wilayah Desa
 // Fitur ini penting: Peta otomatis pas ukurannya ke batas desa saat pertama dibuka
 map.fitBounds(geojsonLayer.getBounds());
